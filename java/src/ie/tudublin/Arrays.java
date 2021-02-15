964db23be51cb5843762cb7eca983abbf31c8aaf
SiuDzoen <C19401596@mytudublin.ie>
2021-02-15 14:00:05 +0000

    week4 work

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,12 +4,50 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet {
 
+    public float map1(float from, float start1, float stop1, float start2, float stop2){
+        float range1 = stop1 - start1;
+        float range2 = stop2 - start2;
+        float howFar = from - start1;
+
+        return start2 + (howFar / range1) * range2;
+        //return start2 + (stop2 - start2) * ((from - start1) / (stop1 - start1));
+    }
+
+    public void drawGrid(){
+        float border = width * 0.1f;
+        stroke(0, 255, 0);
+        textAlign(CENTER, CENTER);
+        for(int i = -5; i <= 5; i++){
+            float x = map1(i, -5, 5, border, width - border);
+            line(x, border, x, height - border);
+            line(border, x, width - border, x);
+            fill(255,255,255);
+            text(i, x, border * 0.5f);
+            text(i, border * 0.5f, x);
+        }
+    }
+
     public void settings() {
-        size(500, 500);                
+        size(500, 500);
+        float f = map1(2, 0, 10, 0, width);
+        println(f); // should print 100
+        
+        f = map1(9, 0, 1, 0, 10);
+        println(f); // should print 90
+
+        f = map1(250, 200, 300, 400, 500);
+        println(f); // should print 450
+
+        f = map1(5, 0, 10, 1000, 2000);
+        println(f); // should print 1500
     }
 
     int mode = 0;
 
+    float[] rainfall = {45, 37, 55, 27, 38, 50, 79, 48, 105, 31, 100, 58};
+    String[] months = {"Jan", "Feb", "March", "April", "May", "June", "July", "August", "Sept", "Oct", "Nov", "Dec"};
+    float[] arr = new float[100]; // 100 float array
+ 
     public void keyPressed() {
         // the value of mode will be the number of the 
         // number key pressed
@@ -19,12 +57,54 @@ public class Arrays extends PApplet {
 
     public void setup() {
         colorMode(HSB);
+
+        for(int i=0;i<rainfall.length;i++){
+            println(months[i] + "\t" + rainfall[i]);
+        }
+
+        // enhanced for loop
+        for(float f:rainfall){
+            println(f);
+        }
+
+        int min = 0;
+        int max = 0;
+        float sum = 0;
+        for(int i=0;i<rainfall.length;i++){
+            if(rainfall[i] > rainfall[min]){
+                min = i;
+            }
+            if(rainfall[i] > rainfall[max]){
+                max = i;
+            }
+            sum += rainfall[i];
+        }
+
+        float average = sum / (float) rainfall.length;
+        println("Least rainfall was in "+ months[min] + " with " + rainfall[min]);
+        println("Most rainfall was in "+ months[max] + " with " + rainfall[max]);
+        println("Average ranfall: " + average);
+
+        // Draw a bar chart of the rainfall
+        // Use the map function
+
+        float w = width / (float) rainfall.length;
+        for(int i=0;i<rainfall.length;i++){
+            noStroke();
+            fill(random(255), 255, 255);
+            float x = map(i, 0, rainfall.length, 0, width);
+            rect(x, height, w, -rainfall[i]);
+        }
     }
 
     float offset = 0;
 
     public void draw() {
-        background(0);
-        noStroke();
+        //background(0);
+        //drawGrid();
+        colorMode(HSB);
+        //float c = map(mouseX, 0, width, 0, 255);
+        //background(c, 255, 255);
+
     }
 }
