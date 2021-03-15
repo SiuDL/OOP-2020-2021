0dca0354708e9dcd2a611d9f290e0bbe509ebef4
SiuDzoen <C19401596@mytudublin.ie>
2021-03-15 12:44:18 +0000

    week 8 class

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -4,13 +4,15 @@ import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import processing.core.PApplet;
 
 public class Audio2 extends PApplet {
 
-    Minim minin;
-    AudioPlayer ai;
+    Minim minim;
+    AudioPlayer ap;
     AudioBuffer ab;
+    FFT fft;
 
     public void settings() {
         size(512, 512, P3D);
@@ -25,11 +27,25 @@ public class Audio2 extends PApplet {
     public void setup() {         
         colorMode(HSB);
         
+        minim = new Minim(this);
+        ap = minim.loadFile("heroplanet.mp3", width); // arg1 - audio to be processed
+        ab = ap.mix; // ap.mix gets the left and right audio channel
+        
+
+        fft = new FFT(width, 44100);
     }
 
     public void keyPressed() {
         if (keyCode >= '0' && keyCode <= '5') {
             which = keyCode - '0';
+        }
+        if(keyCode == ' '){
+            if(ap.isPlaying()){
+                ap.pause();
+            }else{
+                ap.rewind();
+                ap.play();
+            }
         }
     }
 
@@ -38,5 +54,19 @@ public class Audio2 extends PApplet {
     public void draw() {
         background(0);
         stroke(255);
+
+        float halfHeight = height / 2;
+        for(int i=0;i<ab.size();i++){
+            stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
+            line(i, halfHeight - (ab.get(i) * halfHeight), i, halfHeight + (ab.get(i) * halfHeight));
+        }
+
+        fft.window(FFT.HAMMING);
+        fft.forward(ab);
+
+        for(int i=0;i<fft.specSize();i++){
+            stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
+            line(1, 0, i, fft.getBand(i) * halfHeight);
+        }
     }
 }
